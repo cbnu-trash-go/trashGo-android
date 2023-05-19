7dd312f801b6052b8d21e03ebf01d7d9dec96533
sohenney <ththgus0642@naver.com>
2023-05-19 16:34:43 +0900

    mypage에서 넘어가는 페이지 모두 연결 완료했습니다.

---
+++ app/src/main/java/com/trashgo/app/activity_notification.java
@@ -0,0 +1,12 @@
+package com.trashgo.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_notification extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notificationsettings);
+    }
+}
---
+++ app/src/main/java/com/trashgo/app/activity_profile.java
@@ -0,0 +1,12 @@
+package com.trashgo.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_profile extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
