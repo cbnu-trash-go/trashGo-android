ed7286ccc19d5c05551941f38b33a52a5c99364d
dotoooo22 <dt2gg2@gmail.com>
2023-05-24 14:48:40 +0900

    onclick을 xml에서 등록하는 방식으로 수정, 마이페이지온클릭추가

--- app/src/main/java/com/trashgo/app/MainActivity.java
+++ app/src/main/java/com/trashgo/app/MainActivity.java
@@ -2,24 +2,26 @@ package com.trashgo.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 //주원
 
 public class MainActivity extends AppCompatActivity {
     ImageButton imageView1;
+    ImageView rankTap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView1 = (ImageButton) findViewById(R.id.imageViewtree); //코드로 뷰를불러옴 직접 리스너를 등록함
+        //imageView1 = (ImageButton) findViewById(R.id.imageViewtree); //코드로 뷰를불러옴 직접 리스너를 등록함
+        //rankTap = (ImageView) findViewById(R.id.rankTap);
 
 
+        /* xml에서 onclick연결로 변경
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         imageView1.setOnClickListener(listener);
+        */
+
+    }
+    public void mytreeClick(View v) {
+        Intent intent = new Intent(MainActivity.this, MyploggingdataActivity.class);
+        startActivity(intent);
+    }
+    public void mypageClick(View v) {
+        Intent intent = new Intent(MainActivity.this, activity_mypage.class);
+        startActivity(intent);
     }
 }
