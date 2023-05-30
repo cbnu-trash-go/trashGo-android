aab2d1e6ff18d3b7f2fca4195b630123112de589
PKDGOOD <sean4787@gmail.com>
2023-05-31 07:59:00 +0900

    하단메뉴바 추가

--- app/src/main/java/com/trashgo/app/MainActivity.java
+++ app/src/main/java/com/trashgo/app/MainActivity.java
@@ -1,51 +1,71 @@
 package com.trashgo.app;
 
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.trashgo.app.fragment.FirstFragment;
+import com.trashgo.app.fragment.SecondFragment;
+import com.trashgo.app.fragment.TreeFragment;
 
 //주원
 
+/**
+ * bottomNavigation(하단 메뉴바) 적용 - pkdgood
+ */
 public class MainActivity extends AppCompatActivity {
-    ImageButton imageView1;
-    ImageView rankTap;
+    private Fragment firstFragment, secondFragment, treeFragment;
+
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // sohenney '방해 금지' 모드 권한 확인
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            if (!notificationManager.isNotificationPolicyAccessGranted()) {
-                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
-                startActivity(intent);
-            }
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//            if (!notificationManager.isNotificationPolicyAccessGranted()) {
+//                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+//                startActivity(intent);
+//            }
+//        }
 
-        //imageView1 = (ImageButton) findViewById(R.id.imageViewtree); //코드로 뷰를불러옴 직접 리스너를 등록함
-        //rankTap = (ImageView) findViewById(R.id.rankTap);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomnavi);
+        frameLayout = (FrameLayout) findViewById(R.id.framelayout);
+
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+        treeFragment = new TreeFragment();
 
 
-        /* xml에서 onclick연결로 변경
-        View.OnClickListener listener = new View.OnClickListener() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, treeFragment).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MyploggingdataActivity.class);
-                startActivity(intent);
-                // 버튼 동작 설정
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navi_1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, firstFragment).commit();
+                        return true;
+                    case R.id.navi_2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, secondFragment).commit();
+                        return true;
+                    case R.id.navi_3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, treeFragment).commit();
+                        return true;
+                }
+                return false;
             }
-        };
-        imageView1.setOnClickListener(listener);
-        */
+        });
 
     }
     public void mytreeClick(View v) {
---
+++ app/src/main/java/com/trashgo/app/MapActivity.java
@@ -0,0 +1,13 @@
+package com.trashgo.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+    }
+}
