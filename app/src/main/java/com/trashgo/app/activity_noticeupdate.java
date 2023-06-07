f220c5bd02e2afcd967e2b112651e4df12ad3c8e
sohenney <ththgus0642@naver.com>
2023-06-07 17:13:44 +0900

    공지사항이랑 업데이트 페이지 만들었습니다. 근데 fragment 페이지 전환이 너무 어려워요.. 화면 전환 필요합니다. activity_noticeupdate 파일 수정 필요

--- app/src/main/java/com/trashgo/app/activity_noticeupdate.java
+++ app/src/main/java/com/trashgo/app/activity_noticeupdate.java
@@ -1,16 +1,7 @@
 package com.trashgo.app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import com.trashgo.app.fragment.Notice1Fragment;
 
 // sohenney activity_noticeupdate.java 추가
 public class activity_noticeupdate extends AppCompatActivity{
@@ -18,18 +9,5 @@ public class activity_noticeupdate extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.noticesupdates);
-
-        Button btnNotice1 = (Button) findViewById(R.id.btnnotice1);
-        btnNotice1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                Notice1Fragment notice1Fragment = Notice1Fragment.newInstance("param1", "param2");
-                fragmentTransaction.replace(R.id.noticeupdatemain, notice1Fragment);
-                fragmentTransaction.commit();
-                Log.d("MyApp", "버튼이 클릭되었습니다.");
-            }
-        });
     }
 }
