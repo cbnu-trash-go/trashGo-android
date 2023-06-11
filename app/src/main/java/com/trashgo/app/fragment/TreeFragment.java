c51550b5ee6e926fc899a078130c5cbdc7dbf9de
dotoooo22 <dt2gg2@gmail.com>
2023-06-11 15:30:10 +0900

    tree충돌해결

--- app/src/main/java/com/trashgo/app/fragment/TreeFragment.java
+++ app/src/main/java/com/trashgo/app/fragment/TreeFragment.java
@@ -3,6 +3,9 @@ package com.trashgo.app.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,25 +13,26 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.ViewFlipper;
-
-import androidx.fragment.app.Fragment;
-
 import com.trashgo.app.MainActivity;
 import com.trashgo.app.R;
-import com.trashgo.app.aiActivity;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import com.trashgo.app.aiActivity;
 
+/**
+ * 생성 - pkdgood
+ */
 public class TreeFragment extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
 
     private ViewFlipper viewFlipper;
     private ProgressBar progressBar;
@@ -38,13 +42,21 @@ public class TreeFragment extends Fragment {
 
     private List<String> buttonTexts;
     private int currentIndex = 0;
-
     MainActivity mainActivity;
 
     public TreeFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TreeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static TreeFragment newInstance(String param1, String param2) {
         TreeFragment fragment = new TreeFragment();
         Bundle args = new Bundle();
@@ -62,14 +74,24 @@ public class TreeFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+
+    /**
+     * by dotom
+     */
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tree, container, false);
-
         viewFlipper = view.findViewById(R.id.imageViewtree);
-        //viewFlipper.setFlipInterval(3000); // 이미지 전환 간격 (3초)
-        //viewFlipper.startFlipping(); // 자동으로 이미지 전환 시작
-
         viewFlipper.addView(createImageView(R.drawable.seed));
         viewFlipper.addView(createImageView(R.drawable.sprout));
         viewFlipper.addView(createImageView(R.drawable.treemain));
@@ -108,6 +130,18 @@ public class TreeFragment extends Fragment {
             }
         });
 
+        view.findViewById(R.id.aibutton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), aiActivity.class);
+                startActivity(intent);
+
+            }
+
+
+
+        });
+
         return view;
     }
 
@@ -127,43 +161,11 @@ public class TreeFragment extends Fragment {
         buttonTexts.add("더러워진 손씻기");
         buttonTexts.add("플로깅 30분이상하기");
         Collections.shuffle(buttonTexts); // 텍스트를 섞음
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-
-
-    /**
-     * by dotom
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_tree, container, false);
-
-        view.findViewById(R.id.aibutton).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), aiActivity.class);
-                startActivity(intent);
-
-            }
-
-
-
-        });
-
-        return view;
-
-    }
-
     private void setNextButtonText() {
         button.setText(buttonTexts.get(currentIndex));
         currentIndex = (currentIndex + 1) % buttonTexts.size();
+    }
+    public void mytreeClick(View view) {
     }
 }
